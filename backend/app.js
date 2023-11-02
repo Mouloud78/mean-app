71375d0502c86252c7209f9836d610acf69acc27
Mouloud78 <mouloudouldali78@gmail.com>
2023-11-02 12:49:53 -0400

    add endpoint get post

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,35 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("First middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-header",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server",
+    },
+    {
+      id: "ksaflaj12421l",
+      title: "Second server-side post",
+      content: "This is coming from the server",
+    },
+  ];
+  res.status(200).json({
+    message: "Post fetched succesfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
