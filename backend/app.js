e4a2294634c531a8c91a9111140aeb50ce5418a3
Mouloud78 <mouloudouldali78@gmail.com>
2023-11-03 10:27:18 -0400

    setHeader

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 //import express
 const express = require("express");
+const bodyParser = require("body-parser");
 
 //use express
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-header",
+    "Access-Control-Allow-headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
@@ -17,7 +20,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/posts", (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully",
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
   const posts = [
     {
       id: "fadf12421l",
